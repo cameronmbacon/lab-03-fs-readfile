743d814a272598f72057cb17d74baab4605964a2
Cameron Bacon <cameronmbacon@gmail.com>
2017-05-02 16:34:53 -0700

    removed console logs and fixed tests

--- lab-cameron/test/fs-readfile-test.js
+++ lab-cameron/test/fs-readfile-test.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const expect = require('chai').expect;
-const reader = require('../lib/fs-readfile.js');
+const chai = require('chai');
+const expect = chai.expect;
+const reader = require('../lib/fs-readfile');
 
 let filePaths = [
   `${__dirname}/../data/first.txt`,
@@ -9,27 +10,40 @@ let filePaths = [
   `${__dirname}/../data/third.txt`,
 ];
 
-let hexBuffers = reader(filePaths);
-describe('fs-readfile.js', function() {
-  describe('#readFile(\'first.txt\')', function() {
-    it('should read first.txt and return a hex string', function(done) {
-      console.log('8 bytes of first.txt as a hex string', hexBuffers[0]);
-      expect(typeof hexBuffers[0]).equal(typeof 'string');
-      done();
+let testData = ['6669727374207465', '7365636f6e642074', '7468697264207465'];
+
+describe('fs-readfile', function() {
+  describe('#reader(\'first.txt\')', function() {
+    it('should read first.txt and return an 8-byte hex string', function(done) {
+      reader(filePaths, function(hexBuffers) {
+        console.log('8 bytes of first.txt as a hex string', hexBuffers[0]);
+        expect(typeof hexBuffers[0]).equal(typeof 'string');
+        expect(hexBuffers[0]).to.equal(testData[0]);
+        expect(hexBuffers).to.deep.equal(testData);
+        done();
+      });
     });
   });
-  describe('#readFile(\'second.txt\')', function() {
-    it('should read second.txt and return a hex string', function(done) {
-      console.log('8 bytes of second.txt as a hex string', hexBuffers[1]);
-      expect(typeof hexBuffers[1]).equal(typeof 'string');
-      done();
+  describe('#reader(\'second.txt\')', function() {
+    it('should read second.txt and return an 8-byte hex string', function(done) {
+      reader(filePaths, function(hexBuffers) {
+        console.log('8 bytes of second.txt as a hex string', hexBuffers[1]);
+        expect(typeof hexBuffers[1]).equal(typeof 'string');
+        expect(hexBuffers[1]).to.equal(testData[1]);
+        expect(hexBuffers).to.deep.equal(testData);
+        done();
+      });
     });
   });
-  describe('#readFile(\'third.txt\')', function() {
-    it('should read third.txt and return a hex string', function(done) {
-      console.log('8 bytes of third.txt as a hex string', hexBuffers[2]);
-      expect(typeof hexBuffers[2]).equal(typeof 'string');
-      done();
+  describe('#reader(\'third.txt\')', function() {
+    it('should read third.txt and return an 8-byte hex string', function(done) {
+      reader(filePaths, function(hexBuffers) {
+        console.log('8 bytes of third.txt as a hex string', hexBuffers[2]);
+        expect(typeof hexBuffers[2]).equal(typeof 'string');
+        expect(hexBuffers[2]).to.equal(testData[2]);
+        expect(hexBuffers).to.deep.equal(testData);
+        done();
+      });
     });
   });
 });
